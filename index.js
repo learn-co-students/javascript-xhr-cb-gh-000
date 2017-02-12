c242de7c7ddccd4aa61eb0910cdc24558b693b27
Keith Gordon <keithg07@gmail.com>
2017-02-12 03:21:43 +0000

    added functions to access github repos via api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function showRepositories(event, data) {
-  console.log(this.responseText);
-}
-
 function getRepositories() {
   const req = new XMLHttpRequest
   req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/keithgordon/repos')
   req.send()
+}
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList;
 }
