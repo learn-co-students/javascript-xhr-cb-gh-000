e1f816f004e449e138a7f0e5e5774af16eb2e13a
Keith Gordon <keithg07@gmail.com>
2017-02-12 02:41:12 +0000

    added eventListener and div for repos on html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+function showRepositories(event, data) {
+  console.log(this.responseText);
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest
+  req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/keithgordon/repos')
   req.send()
-
 }
