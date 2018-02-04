203036ab9991a02e180f81a3b3db4facdfccf84a
Fatih Intekin <fatihint@gmail.com>
2018-02-04 00:08:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function showRepositories(event, data) {
+    let repos = JSON.parse(this.responseText);
+    console.log(repos);
+    const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+    document.getElementById("repositories").innerHTML = repoList
+
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', showRepositories);
+  req.open("GET", 'https://api.github.com/users/fatihint/repos')
+  req.send()
+}
